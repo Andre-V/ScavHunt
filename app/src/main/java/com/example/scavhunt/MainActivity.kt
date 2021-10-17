020b112e8cb05370f5be9504a0c1e1c8211867b5
Andre Villalon <29933382+Andre-V@users.noreply.github.com>
2021-10-17 22:19:45 +1100

    create working bottom navigation

--- app/src/main/java/com/example/scavhunt/MainActivity.kt
+++ app/src/main/java/com/example/scavhunt/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.scavhunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up navigation
+        val navView : BottomNavigationView = findViewById(R.id.main_nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container)
+            as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+
     }
 }
